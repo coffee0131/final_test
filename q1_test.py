db353528dc14f0369f9f4635679fc6f42f7ee853
coffee0131 <ragnine9@gmail.com>
2020-06-25 19:53:10 +0900

    q1

--- q1_test.py
+++ q1_test.py
@@ -1,26 +1,14 @@
-def square_digits(num):
-    pass
-
-%나머지 //몫
-
 import math
 
 def digit_length(n):
     return int(math.log10(n)) + 1 if n else 0
 
 def square_digits(num):
-    a = num
-    b = digit_length(a)
-    e = 0
-    count = 0
-    while a < 10:
-        c = a%10
-        a = a // num
-        d = c**2
-        e += d
-        count += digit_length(c)
-
-
+	x = 0 ; y = 0; result = 0;
+	for i in str(num):
+		x = (int(i)**2); y = digit_length(result); result += x*(10**y) 
+	return result
 
 def test_q1():
     assert square_digits(9119) == 811181
+    #assert square_digits(9) == 81
