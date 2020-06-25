309c05ed4a72e9fd53746c80165f4b23c7fb8784
coffee0131 <ragnine9@gmail.com>
2020-06-25 20:18:54 +0900

    q3

--- q3_test.py
+++ q3_test.py
@@ -1,5 +1,18 @@
+import socket
+
 def ipv4_address(address):
-    pass
+    try:
+        socket.inet_pton(socket.AF_INET, address)
+    except AttributeError:  # no inet_pton here, sorry
+        try:
+            socket.inet_aton(address)
+        except socket.error:
+            return False
+        return address.count('.') == 3
+    except socket.error:  # not a valid address
+        return False
+
+    return True
 
 def test_q():
     assert ipv4_address("") == False
